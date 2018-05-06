befb1e77deb3bcc199b1ac230c0587d976bf13f5
gamingexpx12 <gamingexpx12@gmail.com>
2018-05-06 11:39:01 +0200

    Second attempt

---
+++ Calcium2.py
@@ -0,0 +1,35 @@
+from tkinter import *
+
+
+class CalciumApp:
+    def layout(self):
+        self.results = Label(self.root, text ="Results")
+        self.results.grid(column = 0, row = 0, columnspan = 3)
+
+        self.button1 = Button(self.root, text = "1")
+        self.button1.grid(column = 0, row = 1)
+
+        self.button2 = Button(self.root, text = "2")
+        self.button2.grid(column = 1, row = 1)
+
+        self.button3 = Button(self.root, text = "3")
+        self.button3.grid(column = 2, row = 1)
+
+
+    def __init__(self, root):
+        self.root = root
+        self.layout()
+
+
+    def run(self):
+        mainloop()
+
+
+def main():
+    root = Tk()
+    app = CalciumApp(root)
+    app.run()
+
+
+if __name__ == '__main__':
+    main()
